55685bf0d75c5e0f8ec7ceb8663181a0d7ef1cdf
mamiprogramming <mamiprogramming@gmail.com>
2024-03-18 01:52:28 +0900

    venueアクション・ルーティング

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,10 @@
 class VenuesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
+  validates :user_name, presence: true
+  validates :prefecture_id, presence: true
+  validates :prefecture_id,
+            numericality: { other_than: 1, message: "を入力してください" }
 end
